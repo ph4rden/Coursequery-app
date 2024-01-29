020540b91f72b40750600c33ddf159e0423f116f
Brian <brianphan17@yahoo.com>
2024-01-28 22:02:03 -0600

    added routing and dashboard page

--- coursequery-frontend/src/main.tsx
+++ coursequery-frontend/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Dashboard from './pages/Dashboard.tsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
