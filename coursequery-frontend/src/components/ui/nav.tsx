949f85cdd224097be5557aaad3881d9424dd3046
jidaho <jidahosa1@gmail.com>
2024-03-15 18:19:24 -0500

    contact us page

--- coursequery-frontend/src/components/ui/nav.tsx
+++ coursequery-frontend/src/components/ui/nav.tsx
@@ -18,7 +18,7 @@ const Nav: React.FC<NavProps> = () => {
     navigate("/login"); // Navigate to the "/login" page
   };
   const handleLogoutClick = () => {
-    navigate("/landingpage"); // Navigate to the "/login" page
+    navigate("/"); // Navigate to the "landing page" page
   };
 
   return (
@@ -26,7 +26,9 @@ const Nav: React.FC<NavProps> = () => {
       className={`max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4`}
     >
       <div className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        Coursequery
+        <a href="/" className="cursor-pointer">
+          Coursequery
+        </a>
       </div>
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul className="font-medium flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse items-center">
@@ -57,7 +59,7 @@ const Nav: React.FC<NavProps> = () => {
             <>
               <li>
                 <a
-                  href="#"
+                  href="/contactus"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   aria-current="page"
                 >
