6d417bfe4f57d5d760c1942a8aebef011f9877f2
jidaho <jidahosa1@gmail.com>
2024-03-17 17:43:07 -0500

    names

--- coursequery-frontend/src/components/ui/contactusform.tsx
+++ coursequery-frontend/src/components/ui/contactusform.tsx
@@ -6,8 +6,6 @@ import { Textarea } from "@/components/ui/textarea";
 export default function ContactUsForm() {
   return (
     <div className="bg-white border-2 border-gray-300 dark:border-gray-700 p-4 rounded-md shadow-md space-y-8 mx-auto max-w-md">
-      {" "}
-      {/* Added mx-auto and max-w-md */}
       <div className="space-y-2">
         <p className="text-gray-500 dark:text-gray-400">
           Please fill the below form, and we will get back to you as soon as
@@ -15,9 +13,35 @@ export default function ContactUsForm() {
         </p>
       </div>
       <div className="space-y-4">
-        <div className="space-y-2">
-          <Label className="text-gray-600 dark:text-gray-400" htmlFor="name">
-            Email Address
+        <div className="flex space-x-4">
+          <div className="space-y-2 flex-1 text-left">
+            <Label
+              className="text-gray-600 dark:text-gray-400"
+              htmlFor="firstName"
+            >
+              First Name
+            </Label>
+            <Input
+              className="border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800"
+              id="firstName"
+            />
+          </div>
+          <div className="space-y-2 flex-1 text-left">
+            <Label
+              className="text-gray-600 dark:text-gray-400"
+              htmlFor="lastName"
+            >
+              Last Name
+            </Label>
+            <Input
+              className="border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800"
+              id="lastName"
+            />
+          </div>
+        </div>
+        <div className="space-y-2 text-left">
+          <Label className="text-gray-600 dark:text-gray-400" htmlFor="email">
+            Email Address*
           </Label>
           <Input
             className="border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800"
